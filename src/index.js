4f6b65f7f374f5a0f7ce67140964e144f9d4ef7a
anjali-patelnz <anjali.c.patel@gmail.com>
2020-12-26 13:58:24 +1300

    Updated JS for convert to farenheit function, adjusted search bar function and added a default city

--- src/index.js
+++ src/index.js
@@ -61,19 +61,27 @@ function showWeather(response) {
   tempLo.innerHTML = ` ${minTemp}`;
   humidity.innerHTML = `${response.data.main.humidity}`;
   windspeed.innerHTML = `${wind} `;
+
+  mainCelciTemp = response.data.main.temp;
+  mainCelciTempHi = response.data.main.temp_max;
+  mainCelciTempLo = response.data.main.temp_min;
 }
 
-function displayCity(event) {
-  event.preventDefault();
-  let myCity = document.querySelector("#city-search").value;
+function findWeather(city) {
   let metric = "metric";
   let apiKey = "c31b4fce1a46009ae0af063ea44bb353";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=${apiKey}&units=${metric}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${metric}`;
   axios.get(url).then(showWeather);
 }
 
+function searchbarCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  findWeather(city);
+}
+
 let changeCity = document.querySelector("#search-form");
-changeCity.addEventListener("submit", displayCity);
+changeCity.addEventListener("submit", searchbarCity);
 
 function retrievePosition(position) {
   let apiKey = "c31b4fce1a46009ae0af063ea44bb353";
@@ -93,24 +101,32 @@ myWeather.addEventListener("click", findPosition);
 
 function displayCelcius(event) {
   event.preventDefault();
-  let myCity = document.querySelector("#city-search").value;
-  let unit = "metric";
-  let apiKey = "c31b4fce1a46009ae0af063ea44bb353";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=${apiKey}&units=${unit}`;
-  axios.get(url).then(showWeather);
 }
 
-let degreeCelci = document.querySelector("#celcius-button");
+let degreeCelci = document.querySelector("#celciusButton");
 degreeCelci.addEventListener("click", displayCelcius);
 
 function displayFarenheit(event) {
   event.preventDefault();
-  let myCity = document.querySelector("#city-search").value;
-  let unit = "imperial";
-  let apiKey = "c31b4fce1a46009ae0af063ea44bb353";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&appid=${apiKey}&units=${unit}`;
-  axios.get(url).then(showWeather);
+  let mainTemp = document.querySelector("#mainTemp");
+  let tempHi = document.querySelector(".mainHi");
+  let tempLo = document.querySelector(".mainLo");
+
+  let mainFarenTemp = (mainCelciTemp * 9) / 5 + 32;
+  let convertFarenMain = Math.round(mainFarenTemp);
+  let mainFarenHi = (mainCelciTempHi * 9) / 5 + 32;
+  let convertFarenHi = Math.round(mainFarenHi);
+  let mainFarenLo = (mainCelciTempLo * 9) / 5 + 32;
+  let convertFarenLo = Math.round(mainFarenLo);
+
+  mainTemp.innerHTML = `${convertFarenMain}°`;
+  tempHi.innerHTML = ` ${convertFarenHi}`;
+  tempLo.innerHTML = ` ${convertFarenLo}`;
 }
 
-let degreeFaren = document.querySelector("#farenheit-button");
+let mainCelciTemp = null;
+
+let degreeFaren = document.querySelector("#farenheitButton");
 degreeFaren.addEventListener("click", displayFarenheit);
+
+findWeather("Wellington");
